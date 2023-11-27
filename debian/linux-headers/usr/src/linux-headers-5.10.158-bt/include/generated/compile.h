f54c8e40e22478e6bbdb197c0e2cbbbc0566fd9a
Manuel Mancera <sinkmanu@gmail.com>
2023-11-27 10:41:40 +0100

    First commit with all sources

---
+++ debian/linux-headers/usr/src/linux-headers-5.10.158-bt/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 101 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#101 SMP Fri May 19 11:01:13 CEST 2023"
+#define LINUX_COMPILE_BY "evaluador"
+#define LINUX_COMPILE_HOST "debian"
+#define LINUX_COMPILER "gcc (Debian 10.2.1-6) 10.2.1 20210110, GNU ld (GNU Binutils for Debian) 2.35.2"
